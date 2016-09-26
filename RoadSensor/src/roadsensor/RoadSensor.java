e7c9cbca3bec8bbab18bff95491f44527abaf755
ElieKassis <elie.cassis@gmail.com>
2016-09-26 21:18:27 +0300

    Road Sensor

---
+++ RoadSensor/src/roadsensor/RoadSensor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roadsensor;
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mlhit10
+ */
+public class RoadSensor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
