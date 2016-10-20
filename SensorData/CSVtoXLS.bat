4d1ba80c35278db37feefee2de295537e5fe0bd6
ElieKassis <elie.cassis@gmail.com>
2016-10-20 13:50:39 +0300

    updates

---
+++ SensorData/CSVtoXLS.bat
@@ -0,0 +1,15 @@
+﻿@echo off
+REM Application default location varies depending on 32 or 64 bit operating system. Assign accordingly
+IF  EXIST "%PROGRAMFILES(X86)%" (
+set CX="%ProgramFiles(x86)%\Softinterface, Inc\Convert XLS\ConvertXLS.EXE"
+) ELSE (
+set CX="C:\Program Files\Softinterface, Inc\Convert XLS\ConvertXLS.EXE"
+)
+
+REM Assign Input and Output folders
+set InputFolder=C:\Users\mlhit10\Desktop\SensorData
+set OutputFolder=C:\Users\mlhit10\Desktop\SensorData
+
+REM CONVERT CSV to XLS WITHOUT MS Excel
+%CX% /S%InputFolder%\*.csv /T%OutputFolder%\*.XLS /F6 /C-4143 /M2 /V
+@echo off
---
+++ SensorData/InsertData.bat
@@ -0,0 +1,13 @@
+@echo off
+type C:\Users\mlhit10\Desktop\SensorData\S1_1.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S1_2.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S2_1.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S2_2.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S3_1.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S3_2.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S4_1.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S4_2.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S5_1.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S5_2.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S6_1.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
+type C:\Users\mlhit10\Desktop\SensorData\S6_2.csv >> C:\Users\mlhit10\Desktop\SensorData\all.csv
